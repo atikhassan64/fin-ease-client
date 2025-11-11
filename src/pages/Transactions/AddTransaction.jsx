00bd10c16333d77dfe822a1ad4939733483c54e3
Atik Hassan <atikhassant64@gmail.com>
2025-11-12 00:16:06 +0600

    added the data post in mongodb

--- src/pages/Transactions/AddTransaction.jsx
+++ src/pages/Transactions/AddTransaction.jsx
@@ -1,19 +1,60 @@
-import React from 'react';
+import React, { use } from 'react';
+import { AuthContext } from '../../context/AuthContext';
 
 const AddTransaction = () => {
+    const { user } = use(AuthContext);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const type = e.target.type.value;
+        const name = e.target.name.value;
+        const date = e.target.date.value;
+        const category = e.target.category.value;
+        const email = e.target.email.value;
+        const amount = e.target.amount.value;
+        const description = e.target.description.value;
+
+        const newTransaction = {
+            type: type,
+            category: category,
+            amount: amount,
+            description: description,
+            date: date,
+            email: email,
+            name: name
+        }
+
+        fetch("http://localhost:3000/transactions", {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newTransaction)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('after data :', data)
+            })
+
+
+        console.log(type, name, date, category, email, amount, description, newTransaction);
+        console.log("this is working")
+
+    }
+
     return (
         <div className='max-w-[1200px] mx-auto px-20'>
             <div className=" mx-auto mt-10 p-6 bg-white rounded-xl shadow-lg border border-amber-100">
                 <h2 className="text-2xl font-semibold mb-6 text-primary">
                     Add Transaction
                 </h2>
-                <form className="">
+                <form onSubmit={handleSubmit} className="">
                     <div className='flex justify-between items-center gap-4'>
                         {/* Left */}
                         <div>
                             {/* Type */}
                             <label className="label">Type</label>
-                            <select className="w-full border border-amber-100 p-2 mb-4 outline-none ">
+                            <select name='type' className="w-full border border-amber-100 p-2 mb-4 outline-none ">
                                 <option>Income</option>
                                 <option>Expense</option>
                             </select>
@@ -22,7 +63,9 @@ const AddTransaction = () => {
                             <label className="label">Name</label>
                             <input
                                 type="text"
+                                name='name'
                                 placeholder="User Name"
+                                value={user.displayName}
                                 readOnly
                                 className="w-full border border-amber-100 p-2 mb-4 outline-none"
                             />
@@ -31,6 +74,7 @@ const AddTransaction = () => {
                             <label className="label">Date</label>
                             <input
                                 type="date"
+                                name='date'
                                 className="w-full border p-2 border-amber-100 outline-none"
                             />
                         </div>
@@ -39,7 +83,7 @@ const AddTransaction = () => {
                         <div>
                             {/* Category */}
                             <label className="label">Category</label>
-                            <select className="w-full border p-2 border-amber-100 mb-4 outline-none">
+                            <select name='category' className="w-full border p-2 border-amber-100 mb-4 outline-none">
                                 <option>Select Category</option>
                                 <option>Salary</option>
                                 <option>Food</option>
@@ -52,7 +96,9 @@ const AddTransaction = () => {
                             <label className="label">Email</label>
                             <input
                                 type="email"
+                                name='email'
                                 placeholder="User Email"
+                                value={user.email}
                                 readOnly
                                 className="w-full border p-2 border-amber-100 mb-4 outline-none"
                             />
@@ -61,6 +107,7 @@ const AddTransaction = () => {
                             <label className="label">Amount</label>
                             <input
                                 type="number"
+                                name='amount'
                                 placeholder="Enter Amount"
                                 className="w-full border p-2 border-amber-100 outline-none"
                             />
@@ -70,15 +117,16 @@ const AddTransaction = () => {
                     {/* Description */}
                     <label className="label mt-5">Description</label>
                     <textarea
+                        name='description'
                         placeholder="Description"
                         className="w-full border p-2 border-amber-100 outline-none"
                     ></textarea>
 
                     {/* Submit button */}
-                    <input 
-                    type="submit" 
-                    value="Add Transaction"
-                    className='btn btn-outline w-full border-amber-100 hover:bg-accent mt-4'
+                    <input
+                        type="submit"
+                        value="Add Transaction"
+                        className='btn btn-outline w-full border-amber-100 hover:bg-accent mt-4'
                     />
                 </form>
             </div>
